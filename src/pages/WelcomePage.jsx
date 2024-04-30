d46ca26ab1daa8bec34138c14ed71fc5bc4be9c7
pengkyu <ilhamsofeanazarudin@gmail.com>
2024-05-01 00:24:33 +0900

    welcome

--- src/pages/WelcomePage.jsx
+++ src/pages/WelcomePage.jsx
@@ -10,26 +10,43 @@ const WelcomePage = ({ onPageChange }) => {
   }
 
   return (
-    <div className="welcome-page">
-      <div className="div">
-        <div className="text-wrapper">SUNGKYUNKWAN UNIVERSITY</div>
-        <img className="kingograd-logo" alt="Kingograd logo" src={logo} />
-        <div className="kingograd-text">ingoGrad</div>
-        <button className="learn-more-button">
-          <div className="overlap-group">
-            <div className="rectangle" />
-            <div className="text-wrapper-2">Learn More</div>
-            </div>
-            </button>
-            <button className="login-button"onClick={() => onPageChange('login')}>
-              <div className="overlap">
-                <div className="text-wrapper-3">Login</div>
-                <img className="person-circle" alt="Person circle" src={loginlogo} />
-                </div>
-                </button>
-                </div>
-                </div>
-                );
-              };
+    // <div className="welcome-page">
+    //   <button className="login-button"onClick={() => onPageChange('login')}>
+    //       <div className="overlap">
+    //         <div className="text-wrapper-3">
+    //           Login
+    //         </div>
+    //         <img className="person-circle" alt="Person circle" src={loginlogo} />
+    //       </div>
+    //   </button>
+    //   <div className="div">
+    //     <div className="text-wrapper">
+    //       SUNGKYUNKWAN UNIVERSITY
+    //     </div>
+    //     <img className="kingograd-logo" alt="Kingograd logo" src={logo} />
+    //     <div className="kingograd-text">
+    //       ingoGrad
+    //     </div>
+    //     <button className="learn-more-button">
+    //       <div className="overlap-group">
+    //         <div className="rectangle" />
+    //         <div className="text-wrapper-2">
+    //           Learn More
+    //         </div>
+    //       </div>
+    //     </button>
+        
+    //   </div>
+    // </div>
+    <div className='welcome-page'>
+      <div className='login-div'>
+        <button className="login-button"onClick={() => onPageChange('login')}>
+           <img className="person-circle" alt="Person circle" src={loginlogo} />
+            <div className="login-text">Login</div>
+         </button>
+      </div>
+    </div>
+  );
+};
 
 export default WelcomePage;
