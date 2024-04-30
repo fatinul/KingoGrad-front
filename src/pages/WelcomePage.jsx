73ea2153896c066225a0f1ae3fa0cb28a8e7cffb
pengkyu <ilhamsofeanazarudin@gmail.com>
2024-04-30 16:59:38 +0900

    WelcomePage

--- src/pages/WelcomePage.jsx
+++ src/pages/WelcomePage.jsx
@@ -1,4 +1,7 @@
 import React from 'react';
+import "./WelcomePage.css"
+import logo from './kingograd_logo-removebg-preview.png';
+import loginlogo from '../assets/PersonCircle.png';
 
 const WelcomePage = ({ onPageChange }) => {
   const handleScrollDown = () => {
@@ -7,12 +10,26 @@ const WelcomePage = ({ onPageChange }) => {
   }
 
   return (
-    <div>
-      <button onClick={() => onPageChange('login')}>Login</button>
-      <h1>KingoGrad</h1>
-      <button onClick={handleScrollDown}>Learn More</button>
-    </div>
-  );
-};
+    <div className="welcome-page">
+      <div className="div">
+        <div className="text-wrapper">SUNGKYUNKWAN UNIVERSITY</div>
+        <img className="kingograd-logo" alt="Kingograd logo" src={logo} />
+        <div className="kingograd-text">ingoGrad</div>
+        <button className="learn-more-button">
+          <div className="overlap-group">
+            <div className="rectangle" />
+            <div className="text-wrapper-2">Learn More</div>
+            </div>
+            </button>
+            <button className="login-button"onClick={() => onPageChange('login')}>
+              <div className="overlap">
+                <div className="text-wrapper-3">Login</div>
+                <img className="person-circle" alt="Person circle" src={loginlogo} />
+                </div>
+                </button>
+                </div>
+                </div>
+                );
+              };
 
 export default WelcomePage;
