792fb7b2a16ae387ad158cb5c6f2b03da156c04a
SKween <nurqistinasuhaimi@gmail.com>
2024-04-30 22:42:12 +0900

    ubah user login

--- src/pages/UserLoginPage.jsx
+++ src/pages/UserLoginPage.jsx
@@ -27,85 +27,44 @@ const UserLoginPage = ({ onPageChange }) => {
 
   return (
     <div className="container">
-      <button type="button" onClick={() => onPageChange('welcome')}>Go Back</button>
-      <h1>User Login</h1>
-      <form>
-        <div>
-          <img className="user" src={image1} alt="user" />
-          <label>Username:</label>
+      <button className="button" onClick={() => onPageChange('welcome')}>Go Back</button>
+      <div className='title'>
+        <h2>User Login</h2>
+      </div>
+      
+      <form className="form-container">
+        <div className='username'>
+          <img className="image" src={image1} alt="user" />
           <input
+            className="username-input"
             type="text"
+            placeholder="Username"
             value={username}
             onChange={handleUsernameChange}
           />
         </div>
-        <div>
-          <img className="pass" src={image2} alt="pass" />
-          <label>Password:</label>
+        <div className='password'>
+          <img className="image" src={image2} alt="pass" />
           <input
+            className='="password-input'
             type="password"
+            placeholder="Password"
             value={password}
             onChange={handlePasswordChange}
           />
         </div>
-        <button className="button" type="button" onClick={handleLogin}>Login</button>
-        <p onClick={() => onPageChange('forgot')} style={{ cursor: 'pointer', color: 'blue' }}>Forgot Password?</p>
+        <div className='login'>
+          <button className="login-button" type="button" onClick={handleLogin}>Login</button>
+        </div><div className='forgot-password'>
+          <p 
+            onClick={() => onPageChange('forgot')} 
+          >
+            Forgot Password?
+          </p>
+        </div>
       </form>
     </div>
   );
-};
+ };
 
 export default UserLoginPage;
-
-// import React, { useState } from 'react';
-// import "../styles/LoginForm.css"
-// import image1 from '../../assets/user.png'; // Import the image file
-// import image2 from '../../assets/pass.png'; // Import the image file
-
-
-// const LoginForm = ({ onLogin, onForgotPassword }) => {
-//   const [username, setUsername] = useState('');
-//   const [password, setPassword] = useState('');
-
-//   const handleSubmit = (event) => {
-//     event.preventDefault();
-//     if (username === 'user' && password === 'pass') {
-//       onLogin(true);
-//     } else {
-//       alert('Incorrect username or password!');
-//       onLogin(false);
-//     }
-//   };
-
-//   return (
-//     <div className="container">
-//       <h2>User Login</h2>     
-      
-//       <form onSubmit={handleSubmit}>
-//         <div>
-//           <img className="user" src={image1} alt="user" />
-//           <input
-//             type="text"
-//             placeholder="Username"
-//             value={username}
-//             onChange={e => setUsername(e.target.value)}
-//           />
-//         </div>
-//         <div>
-//         <img className="pass" src={image2} alt="pass" />
-//           <input
-//             type="password"
-//             placeholder="Password"
-//             value={password}
-//             onChange={e => setPassword(e.target.value)}
-//           />
-//         </div>
-//         <button className="button">Login</button>
-
-//         <button className="forgot-password" onClick={onForgotPassword}>Forgot password?</button>
-//       </form>
-//     </div>
-//   );
-// };
-
-// export default LoginForm;
